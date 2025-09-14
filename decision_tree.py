d799cb45cfbd0ffac4d6ccb2c16cc0edacebd036
Kamlesh Suthar <kamleshsuthar637895@gmail.com>
2025-09-14 12:51:31 +0530

    Update decision_tree.py

--- decision_tree.py
+++ decision_tree.py
@@ -9,3 +9,7 @@ clf = clf.fit(features,labels)
 prediction = clf.predict([[150,1]])
 type(prediction)
 print(f"The fruit is {prediction[0]}")
+from sklearn.tree import plot_tree
+import matplotlib.pyplot as plt
+plt.figure(figsize=(11,6))
+plot_tree(clf, feature_names=["weight","color"], class_names=["apple","orange"], filled=True )
